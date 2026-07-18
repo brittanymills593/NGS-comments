5f116c256e0956714f357c7fee4038a12540969e
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-07-18 16:16:39 +0100

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -225,7 +225,7 @@ def run_new_dashboard ():
                                ).ratio()
            
                                # Group similar comments
-                               if similarity > 0.95:
+                               if similarity > 0.9:
                                    matching_genes.append(gene2)
                                    used_indices.add(j)
            
