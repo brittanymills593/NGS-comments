beb8d681c48cdd85dcbaad256d78f2c31a1a84c0
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-07-18 13:44:46 +0100

    Update Test_NGS_dashboard.py

--- Test_NGS_dashboard.py
+++ Test_NGS_dashboard.py
@@ -69,7 +69,7 @@ def aml_reminder_popup():
     st.write(
         """
         Remember to manually check **UBTF** for partial tandem duplication. Instructions on iPassport:
-        **GEN-SOP 850: NGS analysis and Webserver Instructions (Version 2.0)**
+        GEN-SOP 850: NGS analysis and Webserver Instructions (Version 2.0)
 
         Coordinates (GRCh38):
         **chr17:44,210,790-44,210,949**
