d3fda72bc4613d782f1454be9f1747a799102683
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-07-24 11:12:15 +0100

    Update Test_NGS_dashboard.py

--- Test_NGS_dashboard.py
+++ Test_NGS_dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 from test_updates_dashboard.py import run_new_dashboard
-from new_dashboard import run_new_dashboard
+from old_dashboard import run_old_dashboard
 
 
 version = st.radio(
