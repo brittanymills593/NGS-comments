231e737ff87d90654c02f969bfe7a70d977a48f6
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-08-05 16:13:30 +0100

    Update Test_NGS_dashboard.py

--- Test_NGS_dashboard.py
+++ Test_NGS_dashboard.py
@@ -2,24 +2,33 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-from old_dashboard import run_old_dashboard
 from new_dashboard import run_new_dashboard
 
-
-version = st.radio(
-    "Select dashboard version:",
-    [
-        "Original version",
-        "Updated version"
-    ],
-    horizontal=True
-)
+run_new_dashboard()
 
 
-if version == "Original version":
+# import streamlit as st
 
-    run_old_dashboard()
+# st.set_page_config(layout="wide")
 
-else:
+# from old_dashboard import run_old_dashboard
+# from new_dashboard import run_new_dashboard
 
-    run_new_dashboard()
+
+#version = st.radio(
+#    "Select dashboard version:",
+#    [
+#        "Original version",
+#        "Updated version"
+#    ],
+#    horizontal=True
+#)
+
+
+#if version == "Original version":
+
+#    run_old_dashboard()
+
+#else:
+
+#    run_new_dashboard()
