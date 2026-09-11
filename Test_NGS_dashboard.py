1d828ec1abcee0b92d3773899523b7d02317e7db
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-09-11 14:10:46 +0100

    Update Test_NGS_dashboard.py

--- Test_NGS_dashboard.py
+++ Test_NGS_dashboard.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-from test_updates_dashboard import run_test_dashboard
+from new_dashboard import run_new_dashboard
 
-run_test_dashboard()
+run_new_dashboard()
 
 
 # import streamlit as st
